eec8f31d3fd997be9d06d443dc4e53b3459ff592
Jose-Jardel-Alves <jose.jardel.alves70@gmail.com>
2025-04-12 10:57:54 -0300

    fix(server): escutando o tcp e o udp de forma concorrente e tratando os erros da requisição udp

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import configparser
+import threading
 
 # Função para lidar com a transferência do arquivo
 def handle_transfer(conn, addr):
@@ -40,7 +41,6 @@ def handle_transfer(conn, addr):
             conn.shutdown(socket.SHUT_WR)  # <--- Adicione esta linha
             print(f"[TCP] Enviado {bytes_enviados} bytes")
 
-    # Recebe o ACK
     try:
         ack = conn.recv(1024).decode()
         print(f"[TCP] ACK recebido do cliente: {ack}")
@@ -49,6 +49,41 @@ def handle_transfer(conn, addr):
     finally:
         conn.close()
         print("[TCP] Conexão encerrada com o cliente")
+        
+def handle_connection():
+        
+    while True:
+        mensagem_codificada, endereco = udp_socket.recvfrom(1024)
+        mensagem = mensagem_codificada.decode()
+        print(f"[UDP] Recebido de {endereco}: {mensagem}")
+        
+        
+        response = ""
+        comando, protocolo, nome_arquivo = mensagem.split(",")
+        
+        if mensagem.startswith("REQUEST,TCP,") and nome_arquivo in ['a.txt', 'b.txt']:
+            resposta = f"RESPONSE:,TCP,{TCP_PORT},{nome_arquivo}"
+            udp_socket.sendto(resposta.encode(), endereco)
+            print(f"[UDP] Enviado: {resposta}")
+            
+            conn, addr = tcp_socket.accept()
+            
+            #Thread TCP
+            tcp_thread = threading.Thread(target=handle_transfer,  args=(conn, addr))
+            tcp_thread.daemon = True
+            tcp_thread.start()
+                    
+        else:
+            
+            error = "ERROR, "
+            if comando != "REQUEST":
+                error += "COMANDO INVÁLIDO,,"
+            elif protocolo != "TCP":
+                error += "PROTOCOLO INVÁLIDO,,"
+            else:
+                error += "ARQUIVO INVÁLIDO,,"
+            print(f"[UDP] ERROR | Enviado para {endereco}: {error}")
+            udp_socket.sendto(error.encode(), endereco)
 
 # Lê o arquivo de configuração
 config = configparser.ConfigParser()
@@ -59,32 +94,30 @@ UDP_PORT = int(config["SERVER_CONFIG"]["UDP_PORT"])
 FILE_A = config["SERVER_CONFIG"]["FILE_A"]
 FILE_B = config["SERVER_CONFIG"]["FILE_B"]
 
-# Etapa 1 - Negociação via UDP
+#Socket UDP
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind(("", UDP_PORT))
+udp_socket.bind(("0.0.0.0", UDP_PORT))
 print(f"[UDP] Servidor ouvindo na porta {UDP_PORT}...")
 
-# Etapa 2 - Transferência via TCP
+#Socket TCP
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.bind(("", TCP_PORT))
-tcp_socket.listen(1)
+tcp_socket.bind(("0.0.0.0", TCP_PORT))
+tcp_socket.listen(5)
 print(f"[TCP] Servidor ouvindo na porta {TCP_PORT}...")
 
-    
-# Loop principal do servidor
-while True:
-    # Aguarda requisição UDP
-    mensagem, endereco = udp_socket.recvfrom(1024)
-    mensagem = mensagem.decode()
-    print(f"[UDP] Recebido de {endereco}: {mensagem}")
-    
-    if mensagem.startswith("REQUEST,TCP,"):
-        _, protocolo, nome_arquivo = mensagem.split(",")
-        resposta = f"RESPONSE:,TCP,{TCP_PORT},{nome_arquivo}"
-        udp_socket.sendto(resposta.encode(), endereco)
-        print(f"[UDP] Enviado: {resposta}")
-        conn, addr = tcp_socket.accept()
-        handle_transfer(conn, addr)
-    else:
-        udp_socket.sendto("ERRO")
+
+#Thread UDP
+udp_thread = threading.Thread(target=handle_connection)
+udp_thread.daemon = True
+udp_thread.start()
+
+print("SERIVDOR RODANDO")
+
+
+try:
+    # Mantém o programa principal em execução
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("CONEXÃO COM SERVIDOR ENCERRADA")
     
