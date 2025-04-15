e5863230f30365b51ae71080897737ac8ae9b434
Jose-Jardel-Alves <jose.jardel.alves70@gmail.com>
2025-04-14 21:46:24 -0300

    feat: timeout in client

--- client.py
+++ client.py
@@ -2,78 +2,96 @@ import socket
 import configparser
 import time
 
+def validar_resposta_servidor(resposta):
+    global arquivo_desejado
+    comando, protocolo, porta_transferencia, nome_arquivo = resposta
+    if comando != 'RESPONSE' or nome_arquivo != arquivo_desejado or protocolo != 'TCP':
+        return False
+    return True
+
 
 def iniciar_conexao():
-    # Etapa 1 - Negociação via UDP
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Negociação via UDP
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+        udp_socket.settimeout(1)
+        try:
+            #Envia a requisição
+            mensagem = f"REQUEST,TCP,{arquivo_desejado}"
+            udp_socket.sendto(mensagem.encode(), (servidor, porta_udp))
 
-    #Envia a requisição
-    mensagem = f"REQUEST,TCP,{arquivo_desejado}"
-    udp_socket.sendto(mensagem.encode(), (servidor, porta_udp))
-
-    #Recebe a resosta da requisição
-    resposta, _ = udp_socket.recvfrom(1024)
-    resposta = resposta.decode()
-    print(f"[UDP] Resposta do servidor: {resposta}")
-
-    # Espera algo como "RESPONSE, TCP, 5001, a.txt"
-    try:
-        #validas dados
-        comando, protocolo, porta_transferencia, nome_arquivo = resposta.split(",")
-        porta_transferencia = int(porta_transferencia)
-        print("[UDP] Resposta válida do servidor")
-    except ValueError:
-        udp_socket.close()
-        print("Resposta inválida do servidor.")
-        exit()
-
-    udp_socket.close()
-    return porta_transferencia
+            #Recebe a resosta da requisição
+            resposta, _ = udp_socket.recvfrom(1024)
+            resposta = resposta.decode("utf-8")
+            print(f"[UDP] Resposta do servidor: {resposta}")
+            resposta = resposta.split(",")
+            
+            if (len(resposta) != 4):
+                print("[UDP] Resposta do servidor inválida:")
+                return None
+            elif (not validar_resposta_servidor(resposta)):
+                print("[UDP] Resposta do servidor inválida")        
+                return None
+            else:    
+                comando, protocolo, porta_transferencia, nome_arquivo = resposta
+                porta_transferencia = int(porta_transferencia)
+                print("[UDP] Resposta válida do servidor")
+                return porta_transferencia
+        except socket.timeout as e:
+            print(f"[TCP] ERRO | TIMEOUT UDP: {e}")
 
 def receber_dados(porta_transferencia):    
     
-    # Etapa 2 - Transferência via TCP
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.connect((servidor, porta_transferencia))
-    print(f"[TCP] Conectado ao servidor na porta {porta_transferencia}")
+    # Transferência via TCP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
+        tcp_socket.settimeout(1)
+        try:
+            
+            tcp_socket.connect((servidor, porta_transferencia))
+            print(f"[TCP] Conectado ao servidor na porta {porta_transferencia}")
 
-    # Envia comando get
-    comando = f"get,{arquivo_desejado}"
-    tcp_socket.send(comando.encode())
+            # Envia comando get
+            comando = f"get,{arquivo_desejado}"
+            tcp_socket.sendall(comando.encode("utf-8"))
 
-    # Recebe dados do arquivo
-    conteudo = b""
-    while True:
-        dados = tcp_socket.recv(1024)
-        if not dados:
-            break
-        print(f"Recebido chunk de {len(dados)} bytes")
-        ack_segmento = f"Ack de segmento, recebido {len(dados)} dados"
-        tcp_socket.send(ack_segmento.encode())
-        conteudo += dados
+            # Recebe dados do arquivo
+            conteudo = b""
+            while True:
+                dados = tcp_socket.recv(1024)
+                if not dados:
+                    break
+                print(f"Recebido {len(dados)} bytes")
+                conteudo += dados
 
-    tamanho_recebido = len(conteudo)
+            tamanho_recebido = len(conteudo)
 
-    # Salva arquivo localmente
-    with open(f"baixado_{arquivo_desejado}", "wb") as f:
-        f.write(conteudo)
+            # Salva arquivo localmente
+            with open(f"baixado_{arquivo_desejado}", "wb") as f:
+                f.write(conteudo)
 
-    print(f"[TCP] Arquivo recebido ({tamanho_recebido} bytes) e salvo como baixado_{arquivo_desejado}")
+            print(f"[TCP] Arquivo recebido ({tamanho_recebido} bytes) e salvo como baixado_{arquivo_desejado}")
 
-    # Envia ACK
-    ack = f"[TCP] ftcp_ack,{tamanho_recebido}"
-    tcp_socket.sendall(ack.encode())
-    print(f"[TCP] ACK enviado: {ack}")
+            # Envia ACK
+            ack = f"[TCP] ftcp_ack,{tamanho_recebido}"
+            tcp_socket.sendall(ack.encode("utf-8"))
+            print(f"[TCP] ACK enviado: {ack}")
+        
+        except socket.timeout as e:
+            print(f"[TCP] ERRO | TIMEOUT: {e}")
+        except ConnectionRefusedError as e:
+            print(f"[TCP] ERRO |Conexão recusada {e}")
+        except Exception as e:
+            print(f"[TCP] ERRO | ")
 
-    tcp_socket.close()
     print("[TCP] Conexão encerrada.")
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-
 servidor = "127.0.0.1"
+
 porta_udp = int(config["SERVER_CONFIG"]["UDP_PORT"])
 arquivo_desejado = "b.txt"
 
 porta_transferencia = iniciar_conexao()
-receber_dados(porta_transferencia)
+
+if porta_transferencia != None:
+    receber_dados(porta_transferencia)
--- server.py
+++ server.py
@@ -4,98 +4,108 @@ import configparser
 import threading
 import time
 
-def handle_transfer(conn, addr):
-    print(f"[TCP] Conexão estabelecida com {addr}")
-    conn.settimeout(30)
-    # Recebe o comando "get,arquivo"
-    mensagem = conn.recv(1024).decode()
-    print(f"[TCP] Comando recebido: {mensagem}")
-    comando, nome_arquivo = mensagem.split(",")
     
-    if mensagem.startswith("get,") and nome_arquivo in ['a.txt', 'b.txt']:
+def validar_requisicao_tcp(mensagem):
+    
+    mensagem = mensagem.split(",")
+    
+    if len(mensagem) != 2:
+        raise ValueError("[TCP] ERROR | REQUISIÇÃO INVÁLIDA. Formato <comando> <nome_arquivo>")
+    
+    comando, nome_arquivo = mensagem
+    if comando != 'get':
+        raise ValueError("[TCP] ERROR | COMANDO INVÁLIDO")
+    
+    if nome_arquivo not in ['a.txt', 'b.txt']:
+        raise ValueError("[TCP] ERROR | ARQUIVO INVÁLIDO")
+    
+    if not os.path.exists(nome_arquivo):
+        raise ValueError("[TCP] ERROR | Erro no servidor, arquivo não existe")
         
-        if nome_arquivo == "a.txt":
-            caminho_arquivo = FILE_A 
-        else:
-            caminho_arquivo = FILE_B
-        # Verifica se o arquivo existe
-        if not os.path.exists(caminho_arquivo):
-            erro = "Erro: arquivo não encontrado."
-            print(f"[TCP] {erro}")
-            conn.send(erro.encode('utf-8'))
-        else:
+    return True
+    
+def enviar_dados(conn, addr, arquivo):
+    time.sleep(2)
+    with open(arquivo, "rb") as f:
+            bytes_enviados = 0
+            while True:
+                dados = f.read(1024)
+                if not dados:
+                    break
+                conn.sendall(dados)
+                print(f"[TCP] {len(dados)} dados enviados para {addr}")
+                
+                bytes_enviados += len(dados)
         
-            # Envia o conteúdo do arquivo em segmentos
-            with open(caminho_arquivo, "rb") as f:
-                bytes_enviados = 0
-                while True:
-                    time.sleep(5)
-                    dados = f.read(1024)
-                    if not dados:
-                        break
-                    conn.sendall(dados)
-                    print(f"[TCP] {len(dados)} dados enviados para {addr}")
-                    
-                    acks = conn.recv(1024)
-                    bytes_enviados += len(dados)
-                    if acks:
-                        print(f"[TCP] {acks.decode()} recebeido de {addr}")
-            
             conn.shutdown(socket.SHUT_WR)
             print(f"[TCP] Enviado {bytes_enviados} bytes para {addr}")
-    else:
-        error = "ERROR "
-        
-        if comando != 'get':
-            error += "COMANDO INVÁLIDO"
-        elif nome_arquivo not in ['a.txt', 'b.txt']:
-            error += "ARQUIVO INVÁLIDO"    
-        print(f"[TCP] ERROR Enviado para {addr}: {error}")
-        conn.send(error.encode())
-        
+
+def receber_ack(conn, addr):
     try:
         ack = conn.recv(1024).decode()
         print(f"[TCP] ACK recebido do cliente {addr}: {ack}")
-    except:
-        print(f"[TCP] Erro ao receber ACK do cliente {addr}")
-    finally:
-        conn.close()
+    except Exception as e:
+        print(f"[TCP] Erro ao receber ACK do cliente {addr}. Erro: {e}")
+ 
+def handle_transfer(conn, addr):
+    
+    print(f"[TCP] Conexão estabelecida com {addr}")
+    with conn:
+        # Recebe o comando "get,arquivo"
+        mensagem = conn.recv(1024).decode("utf-8")
+        print(f"[TCP] Comando recebido: {mensagem} de {addr}")
+        
+        try:
+            if validar_requisicao_tcp(mensagem):       
+                comando, arquivo = mensagem.split(",")
+                # Envia o conteúdo do arquivo em segmentos
+                enviar_dados(conn, addr, arquivo)
+                receber_ack(conn, addr)
+                            
+        except ValueError as e:
+            conn.sendall(e)
+        except (ConnectionResetError, ConnectionAbortedError) as e:
+            print(f"[TCP] Conexão com {addr} encerrada durante envio: {e}")
+        
         print(f"[TCP] Conexão encerrada com o cliente {addr}")
+
         
 def handle_connection():
         
     while True:
         mensagem_codificada, endereco = udp_socket.recvfrom(1024)
-        mensagem = mensagem_codificada.decode()
+        mensagem = mensagem_codificada.decode("utf-8")
         print(f"[UDP] Recebido de {endereco}: {mensagem}")
-        time.sleep(10)
         
-        response = ""
-        comando, protocolo, nome_arquivo = mensagem.split(",")
-        
-        if mensagem.startswith("REQUEST,TCP,") and nome_arquivo in ['a.txt', 'b.txt']:
-            resposta = f"RESPONSE,TCP,{TCP_PORT},{nome_arquivo}"
-            udp_socket.sendto(resposta.encode(), endereco)
-            print(f"[UDP] Enviado: {resposta}")
+        requisicao = mensagem.split(",")
+        if (len(requisicao) != 3):
+            error = "ERROR, REQUISIÇÃO INVÁLIDA. FORMATO: <comando>,<protocolo>,<nome_arquivo>"
+            udp_socket.sendto(error.encode("utf-8"), endereco)
             
-            conn, endereco_tcp = tcp_socket.accept()
-            
-            #Thread TCP
-            tcp_thread = threading.Thread(target=handle_transfer,  args=(conn, endereco_tcp))
-            tcp_thread.daemon = True
-            tcp_thread.start()
-                    
         else:
-            
-            error = "ERROR, "
-            if comando != "REQUEST":
-                error += "COMANDO INVÁLIDO,,"
-            elif protocolo != "TCP":
-                error += "PROTOCOLO INVÁLIDO,,"
-            else:
-                error += "ARQUIVO INVÁLIDO,,"
-            print(f"[UDP] ERROR | Enviado para {endereco}: {error}")
-            udp_socket.sendto(error.encode(), endereco)
+            comando, protocolo, nome_arquivo = requisicao
+            if mensagem.startswith("REQUEST,TCP,") and nome_arquivo in ['a.txt', 'b.txt']:
+                resposta = f"RESPONSE,TCP,{TCP_PORT},{nome_arquivo}"
+                udp_socket.sendto(resposta.encode(), endereco)
+                print(f"[UDP] Enviado: {resposta} para {endereco}")
+                
+                conn, endereco_tcp = tcp_socket.accept()
+
+                #Thread TCP
+                tcp_thread = threading.Thread(target=handle_transfer,  args=(conn, endereco_tcp))
+                tcp_thread.daemon = True
+                tcp_thread.start()
+                        
+            else:   
+                error = "ERROR, "
+                if comando != "REQUEST":
+                    error += "COMANDO INVÁLIDO,,"
+                elif protocolo != "TCP":
+                    error += "PROTOCOLO INVÁLIDO,,"
+                else:
+                    error += "ARQUIVO INVÁLIDO,,"
+                print(f"[UDP] ERROR | Enviado para {endereco}: {error}")
+                udp_socket.sendto(error.encode("utf-8"), endereco)
 
 # Lê o arquivo de configuração
 config = configparser.ConfigParser()
@@ -110,14 +120,12 @@ FILE_B = config["SERVER_CONFIG"]["FILE_B"]
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.bind(("0.0.0.0", UDP_PORT))
 
-udp_socket.settimeout(30)
 print(f"[UDP] Servidor ouvindo na porta {UDP_PORT}...")
 
 #Socket TCP
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.bind(("0.0.0.0", TCP_PORT))
 tcp_socket.listen(5)
-tcp_socket.settimeout(30)
 print(f"[TCP] Servidor ouvindo na porta {TCP_PORT}...")
 
 
@@ -129,7 +137,6 @@ udp_thread.start()
 print("SERVIDOR RODANDO")
 
 try:
-    # Mantém o programa principal em execução
     while True:
         pass
 except KeyboardInterrupt:
